49ae1d7f30596acfe6330c09f59f602b2306e73d
isopen <mr_makss@mail.ru>
2016-07-06 10:18:03 +0700

    Update Controller.php

--- app/Controller.php
+++ app/Controller.php
@@ -5,13 +5,17 @@ class Controller {
   protected $db;
 
 	function afterroute() {
+		
 		echo Template::instance()->render('layout.htm');
+		
 	}
 
 	function __construct($f3) {
+		
     $db = new \DB\SQL($f3->get('db'), $f3->get('username'), $f3->get('password'));
     new DB\SQL\Session($db);
     $this->db = $db;
+    
 	}
 
 }
